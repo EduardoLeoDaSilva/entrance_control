2d519c7333377b4e2585da889283a2c37ff686dd
Eduardo Leonardo <eduardo.rosa@terceiroslinx.com.br>
2022-06-25 17:52:09 -0300

    Stylizing homepage

--- lib/main.dart
+++ lib/main.dart
@@ -15,21 +15,60 @@ class HomePage extends StatelessWidget {
         body: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(
+            const Text(
               'You can come in',
               style: TextStyle(
                   color: Colors.white,
                   fontSize: 30,
                   fontWeight: FontWeight.w500),
             ),
-            Text(
-              '0',
-              style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 30,
-                  fontWeight: FontWeight.w500),
+            Padding(
+              padding: const EdgeInsets.all(30),
+              child: const Text(
+                '0',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 30,
+                    fontWeight: FontWeight.w500),
+              ),
             ),
-            Row()
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextButton(
+                  onPressed: null,
+                  child: Text(
+                    "Got in",
+                    style: TextStyle(
+                        color: Colors.blue,
+                        fontSize: 20,
+                        fontWeight: FontWeight.w600),
+                  ),
+                  style: TextButton.styleFrom(
+                      backgroundColor: Colors.white,
+                      fixedSize: const Size(100, 100),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20))),
+                ),
+                SizedBox(
+                  width: 30,
+                ),
+                TextButton(
+                    onPressed: null,
+                    child: const Text(
+                      "Got out",
+                      style: TextStyle(
+                          color: Colors.blue,
+                          fontSize: 20,
+                          fontWeight: FontWeight.w600),
+                    ),
+                    style: TextButton.styleFrom(
+                        backgroundColor: Colors.white,
+                        fixedSize: const Size(100, 100),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20))))
+              ],
+            )
           ],
         ),
       ),
