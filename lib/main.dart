daaf3dc4f321f0adc6581c84d3495cc7abb54279
Eduardo Leonardo <eduardo.rosa@terceiroslinx.com.br>
2022-06-25 17:00:02 -0300

    Creating HomePage, added the texts widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,38 @@
-void main(List<String> args) {}
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(const HomePage());
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'You can come in',
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 30,
+                  fontWeight: FontWeight.w500),
+            ),
+            Text(
+              '0',
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 30,
+                  fontWeight: FontWeight.w500),
+            ),
+            Row()
+          ],
+        ),
+      ),
+    );
+  }
+}
